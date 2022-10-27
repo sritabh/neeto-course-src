81d7f425f3910b05e3e5a3205a71377a4fa59645
Akash Srivastava <akash.srivastava@bigbinary.com>
2022-10-27 22:58:01 +0530

    Added more validations related to matching course metadata and directories

--- validations/src/validation_service.rb
+++ validations/src/validation_service.rb
@@ -2,18 +2,20 @@ require 'yaml'
 require_relative './config_rules/course_name_present'
 require_relative './config_rules/course_slug_present'
 require_relative './config_rules/course_published_present'
+require_relative './config_rules/course_image_exists'
 require_relative './config_rules/course_slugs_unique'
+require_relative './config_rules/courses_metadata_matches_course_directories'
 
 module Src
   class ValidationService
-    attr_reader :app_root, :course_metadata_config, :course_directories
+    attr_reader :app_root, :course_metadata_config, :course_directories, :images_path, :databases_path
 
     def initialize
     end
 
     def process
       puts "Gathering config".blue
-      set_app_root
+      set_directories
       set_course_metadata_config
       set_course_directories
       puts "Validating config".blue
@@ -29,8 +31,11 @@ module Src
 
     private
 
-    def set_app_root
+    def set_directories
       @app_root = File.expand_path("../../", __dir__)
+      assets_path = File.join(app_root, "assets")
+      @images_path = File.join(assets_path, "images")
+      @databases_path = File.join(assets_path, "databases")
     end
 
     def set_course_metadata_config
@@ -38,7 +43,7 @@ module Src
     end
 
     def set_course_directories
-      @course_directories = Dir[File.join(app_root, "courses", "*")]
+      @course_directories = Dir[File.join(app_root, "courses", "*")].select { |f| File.directory? f }
     end
 
     def validate_course_data
@@ -46,8 +51,11 @@ module Src
         validate_course_name_present(course_data, course_index)
         validate_course_slug_present(course_data, course_index)
         validate_course_published_present(course_data, course_index)
+        validate_course_logo_exists_if_specified(course_data, course_index)
+        validate_course_home_logo_exists_if_specified(course_data, course_index)
       end
-      validate_uniqueness_of_course_slugs(course_metadata_config)
+      validate_uniqueness_of_course_slugs
+      validate_course_metadata_config_matches_course_directories
     end
 
     def validate_course_name_present(course_data, course_index)
@@ -62,8 +70,20 @@ module Src
       Src::ConfigRules::CoursePublishedPresent.new(course_data, course_index).process
     end
 
-    def validate_uniqueness_of_course_slugs(courses_data)
-      Src::ConfigRules::CourseSlugsUnique.new(courses_data).process
+    def validate_course_logo_exists_if_specified(course_data, course_index)
+      Src::ConfigRules::CourseImageExists.new(course_data, course_index, "logo", images_path).process
+    end
+
+    def validate_course_home_logo_exists_if_specified(course_data, course_index)
+      Src::ConfigRules::CourseImageExists.new(course_data, course_index, "home_logo", images_path).process
+    end
+
+    def validate_uniqueness_of_course_slugs
+      Src::ConfigRules::CourseSlugsUnique.new(course_metadata_config).process
+    end
+
+    def validate_course_metadata_config_matches_course_directories
+      Src::ConfigRules::CoursesMetadataMatchesCourseDirectories.new(course_metadata_config, course_directories).process
     end
   end
 end
